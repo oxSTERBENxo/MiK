01fdd7c775c575e153cd03134c96d40cb3808c44
RebekaManeva <rebekamaneva@gmail.com>
2025-07-28 20:47:52 -0700

    models and admin

--- djangoProject/myapp/admin.py
+++ djangoProject/myapp/admin.py
@@ -1,3 +1,50 @@
 from django.contrib import admin
+from .models import Country, Question, Choice, Polaroid
+
 
 # Register your models here.
+
+class CountryQuestionInline(admin.TabularInline):
+    model = Question
+    extra = 0
+
+
+class CountryPolaroidInline(admin.TabularInline):
+    model = Polaroid
+    extra = 0
+
+class QuestionChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 0
+
+
+class CountryAdmin(admin.ModelAdmin):
+    inlines = [CountryQuestionInline, CountryPolaroidInline, ]
+    list_display = ("name",)
+
+    def has_add_permission(self, request):
+        return True
+
+    def has_delete_permission(self, request, obj=None):
+        return True
+
+    def has_change_permission(self, request, obj=None):
+        return True
+
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    inlines = [QuestionChoiceInline, ]
+    list_display = ("content",)
+
+class ChoiceAdmin(admin.ModelAdmin):
+    list_display = ("content",)
+
+class PolaroidAdmin(admin.ModelAdmin):
+    list_display = ("headline",)
+
+
+admin.site.register(Country, CountryAdmin)
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice, ChoiceAdmin)
+admin.site.register(Polaroid, PolaroidAdmin)
--- djangoProject/myapp/models.py
+++ djangoProject/myapp/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
 # Create your models here.
+
+class Country(models.Model):
+    name = models.CharField(max_length=100)
+    headline = models.CharField(max_length=100)
+    background = models.ImageField(upload_to="background_photos/", null=True, blank=True)
+    color = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.name}"
+
+class Polaroid(models.Model):
+    country = models.ForeignKey(Country, on_delete=models.CASCADE)
+    headline = models.CharField(max_length=100)
+    photo = models.ImageField(upload_to="polaroid_photos/", null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.country.name}"
+
+class Question(models.Model):
+    country = models.ForeignKey(Country, on_delete=models.CASCADE)
+    content = models.CharField(max_length=200)
+
+    def __str__(self):
+        return f"{self.country.name} {self.content}"
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    content = models.CharField(max_length=200)
+
+    def __str__(self):
+        return f"{self.question} {self.content}"
