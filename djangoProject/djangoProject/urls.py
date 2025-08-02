8b8eb47c53338a896336b34cac6ab85186245962
oxSTERBENxo <eva.knezevik@gmail.com>
2025-08-02 02:25:09 +0200

    zemji + pop-up sreden

--- djangoProject/djangoProject/urls.py
+++ djangoProject/djangoProject/urls.py
@@ -15,7 +15,6 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
 from myapp import views as views
 from django.conf.urls.static import static
 from django.conf import settings
@@ -34,4 +33,3 @@ urlpatterns = [
     }),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
